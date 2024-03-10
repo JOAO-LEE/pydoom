c5d5534b54133ecd826a843239b612d6d5cb043c
João Lee <joaumlimaum@gmail.com>
2024-03-10 04:04:56 -0300

    Added weapon - sprite positions are reversed?

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from raycasting import *
 from object_renderer import *
 from sprite_object import *
 from object_handler import *
+from weapon import *
 
 
 
@@ -25,9 +26,8 @@ class Game:
         self.player = Player(self)
         self.object_renderer = ObjectRenderer(self)
         self.raycasting = RayCasting(self)
-        # self.sprite_object = SpriteObject(self)
-        # self.animated_sprite = AnimatedSprite(self)
         self.object_handler = ObjectHandler(self)
+        self.weapon = Weapon(self)
 
 
 
@@ -35,15 +35,15 @@ class Game:
         self.player.update()
         self.raycasting.update()
         self.object_handler.update()
+        self.weapon.update()
         pg.display.flip()
         self.delta_time =self.clock.tick(FPS)
         pg.display.set_caption(f'f{self.clock.get_fps() : .1f}')
 
     def draw(self):
         # self.screen.fill('black')
-        # self.map.draw()
-        # self.player.draw()
         self.object_renderer.draw()
+        self.weapon.draw()
 
 
     def check_events(self):
---
+++ weapon.py
@@ -0,0 +1,13 @@
+from sprite_object import *
+
+class Weapon(AnimatedSprite):
+    def __init__(self, game, path='resources/sprites/weapons/shotgun/0.png', scale=0.4, animation_time=90):
+        super().__init__(game=game, path=path, scale=scale, animation_time=animation_time)
+        self.images = deque([pg.transform.smoothscale(img, (self.image.get_width() * scale, self.image.get_height() * scale)) for img in self.images])
+        self.weapon_pos = (HALF_WIDTH - self.images[5].get_width() // 2, HEIGHT - self.images[5].get_height())
+
+    def draw(self):
+        self.game.screen.blit(self.images[5], self.weapon_pos)
+    
+    def update(self):
+        pass
